77c2064d8a98fb1c9f571a181789d6d47e0eae67
maojifeng <mjf@51xjzx.com>
2018-03-08 17:00:02 +0800

    add EurekaServer

---
+++ demo-eureka-server/src/main/java/com/example/demo/DemoEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author maojifeng
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaServerApplication.class, args);
+    }
+}
