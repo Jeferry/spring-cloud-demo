8557d3e54db44834d3be19f32d7a78b6c91374c5
maojifeng <mjf@51xjzx.com>
2018-03-09 18:13:51 +0800

    add ribbon consumer

---
+++ demo-consumer/src/main/java/com/example/demo/consumer/DemoConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author maojifeng
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoConsumerApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConsumerApplication.class, args);
+    }
+}
---
+++ demo-consumer/src/main/java/com/example/demo/consumer/consumer/RibbonConsumerController.java
@@ -0,0 +1,36 @@
+/**
+ * YOUTU SOFTWARE Inc.
+ * Copyright (c) 2016-2018 All Rights Reserved.
+ */
+package com.example.demo.consumer.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author maojifeng
+ * @version RibbonConsumerController.java, v 0.1 maojifeng
+ * @date 2018/3/9 17:59
+ * @comment 消费者接口
+ */
+@RestController
+public class RibbonConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 消费调用
+     *
+     * @return
+     */
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+
+
+}
