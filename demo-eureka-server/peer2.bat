00c6eb5db6ec89c646eb07bcf43639f20f2a4e2f
maojifeng <123240527@qq.com>
2018-03-10 13:23:02 +0800

    添加批处理启动

---
+++ demo-eureka-server/peer2.bat
@@ -0,0 +1,2 @@
+call java -jar ./build/libs/demo-eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer2
+pause
---
+++ demo-web/server2.bat
@@ -0,0 +1,2 @@
+call java -jar ./build/libs/demo-web-0.0.1-SNAPSHOT.jar --server.port=8082
+pause
