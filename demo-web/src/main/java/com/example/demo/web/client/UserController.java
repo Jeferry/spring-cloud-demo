8ab2133cc6ad5b6577b0850fffe3270833bb2901
maojifeng <mjf@51xjzx.com>
2018-03-21 16:25:02 +0800

    demo-web add get batch user controller

--- demo-web/src/main/java/com/example/demo/web/client/UserController.java
+++ demo-web/src/main/java/com/example/demo/web/client/UserController.java
@@ -4,6 +4,7 @@
  */
 package com.example.demo.web.client;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.modules.UserVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -27,8 +30,23 @@ public class UserController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @RequestMapping(value = "/users")
+    public List<UserVO> getUserList(Long[] ids) {
+        logger.info("request from /users?id={ids}", JSONObject.toJSONString(ids));
+        List<UserVO> result = new ArrayList<>(8);
+        for (Long id : ids) {
+            result.add(wrapperGetUser(id));
+        }
+        return result;
+    }
+
     @RequestMapping(value = "/users/{id}")
     public UserVO getUserById(@PathVariable Long id) {
+        logger.info("request from /users/{id}", id);
+        return wrapperGetUser(id);
+    }
+
+    private UserVO wrapperGetUser(Long id) {
         logger.info("hello users:id={}", id);
         UserVO userVO = new UserVO();
         userVO.setId(id);
